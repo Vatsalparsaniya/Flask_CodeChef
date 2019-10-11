914b2ac25ddb7ec2ce8b39bdfb44b081ddadfba7
Vatsal <vatsal.pict17@sot.pdpu.ac.in>
2019-10-11 23:38:00 +0530

    About Project

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,25 @@ from flask import Flask,render_template,url_for
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
-    MYSQL_HOST='127.0.0.1',
-    MYSQL_USER='root',
-    MYSQL_PASSWORD='12345678',
-    MYSQL_DB='Flask_CRUD'
+    MYSQL_HOST= 'sql12.freesqldatabase.com',
+    MYSQL_USER='sql12308164',
+    MYSQL_PASSWORD='VPCkMech5s',
+    MYSQL_PORT = 3306,
+    MYSQL_DB='sql12308164'
 )
 
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return render_template('Index.html')
+    cur = mysql.connection.cursor()
+    # cur.execute("CREATE TABLE authorization(USER int(30),EMAIL varchar(35),PASSWORD varchar(30));")
+    cur.execute("INSERT INTO sql12308164.authorization VALUES (\"Darshit\",\"Darshit@gmail.com\",\"12345678\");")
+    cur.execute("SELECT * FROM sql12308164.authorization;")
+    rv = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    return render_template('Index.html',DATA=rv)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=1234)
